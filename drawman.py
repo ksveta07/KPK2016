82ad791fae35342f4bf60160affa54949a91463c
Светлана <kardsvetl2013@gmail.com>
2016-08-16 18:17:50 +0500

    Сделано масштабирование Чертёжника. Ура! Решил issue1

--- drawman.py
+++ drawman.py
@@ -2,12 +2,13 @@ from turtle import Turtle
 
 def init_drawman():
     """ инициализация параметров"""
-    global t, x_current, y_current
+    global t, x_current, y_current, drawman_scale
     t = Turtle()
     t.penup()
     x_current = 0
     y_current = 0
     t.goto(x_current, y_current)
+    drawman_scale = 10
 
 def test_drawman():
     """Тестирование работы Чертёжника
@@ -29,17 +30,15 @@ def pen_up():
 
 def on_vector(dx, dy):
     """ Смещается на вектор"""
-    global x_current, y_current
-    x_current += dx
-    y_current += dy
-    t.goto(x_current, y_current)
+    to_point(x_current + dx, y_current + dy)
+
 
 def to_point(x, y):
     """ Запоминает текущие координаты"""
     global x_current, y_current
     x_current = x
     y_current = y
-    t.goto(x,y)
+    t.goto(drawman_scale*x_current, drawman_scale*y_current)
 
 init_drawman()
 
